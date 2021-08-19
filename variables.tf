6f7b6f671469f7858df493df304457815312a60c
rahulsawant1692 <89205367+rahulsawant1692@users.noreply.github.com>
2021-08-19 12:26:59 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -19,7 +19,7 @@ variable "aws_transit_instance_size" {
 }
 
 variable "aws_transit1_region" {
-  default = "us-east-2"
+  default = "ca-central-1"
 }
 
 variable "aws_transit1_name" {
@@ -35,7 +35,7 @@ variable "aws_spoke_instance_size" {
 }
 
 variable "aws_spoke1_region" {
-  default = "us-east-2"
+  default = "ca-central-1"
 }
 
 variable "aws_spoke1_name" {
