7d1ae226d1e3e36c62baa5b10661bbfdaf94332d
rahulsawant1692 <89205367+rahulsawant1692@users.noreply.github.com>
2021-08-19 10:53:10 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.0.2"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = " Aviatrix-terraform-test"
+    organization = "Aviatrix-terraform-test"
     workspaces {
       name = "ace-iac-day-zero"
     }
